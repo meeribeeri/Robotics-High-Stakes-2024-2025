2c952352211515f5e905c45c2c8736241fda4b53
Alex Ngo <ngoalex9811@gmail.com>
2024-10-12 12:39:48 -0600

    FIRST COMMIT LET US GO also Kyle wants Joshua

---
+++ src/auton.cpp
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+
+std::vector<int[]> readFile(const char* file_name) {
+    FILE* usd_auton_file = fopen(file_name,"r");
+    //left motors, right motors, clamp, 
+}
+
+void autonomous() {
+
+}
